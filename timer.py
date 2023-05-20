da9d7df2f051d6c0d039942843a4b21a9f5cc574
Steph Burton <steph.a.burton@gmail.com>
2023-05-20 12:51:48 -0400

    Condensed to one timer function

--- timer.py
+++ timer.py
@@ -4,26 +4,20 @@ import time
 import subprocess
 
 print("Pomodoro Timer")
-work_time = int(input("How long (minutes) should the work timer run?")) * 60
+work_time = int(input("How long (minutes) should the work timer run? ")) * 60
 
-def work_timer(seconds):
-    print("Running work time for " + str(seconds // 60) + " minutes.")
+def pom_timer(seconds):
+    print("Running timer for " + str(seconds // 60) + " minutes.")
     time.sleep(seconds)
     print("Time's up!")
     subprocess.run(["afplay", "/System/Library/Sounds/Funk.aiff"])
 
-work_timer(work_time)
+pom_timer(work_time)
 
-rest_option = input("Would you like to start a rest timer? y/n")
-
-def rest_timer(seconds):
-    print("Running rest timer for " + str(seconds // 60) + " minutes.")
-    time.sleep(seconds)
-    print("Time's up!")
-    subprocess.run(["afplay", "/System/Library/Sounds/Blow.aiff"])
+rest_option = input("Would you like to start a rest timer? y/n ")
 
 if rest_option == "y":
-    rest_time = int(input("How long (minutes) should the rest timer run?")) * 60
-    rest_timer(rest_time)
+    rest_time = int(input("How long (minutes) should the rest timer run? ")) * 60
+    pom_timer(rest_time)
 else:
     print("Ok. Goodbye.")
